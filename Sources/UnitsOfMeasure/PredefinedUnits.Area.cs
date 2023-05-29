97404b08a43b6bf05cfba1546e60caf6280a59cb
Selene29 <funmailfach@web.de>
2023-05-29 22:41:42 +0200

    add incomplete Area as test

---
+++ Sources/UnitsOfMeasure/PredefinedUnits.Area.cs
@@ -0,0 +1,63 @@
+﻿using System.Numerics;
+
+namespace UnitsOfMeasure;
+
+public struct SquareMeter<TNumber> : IBaseUnit<Meter<TNumber>, TNumber>
+    where TNumber : IMultiplicativeIdentity<TNumber, TNumber>
+{
+    public string Postfix => "m²";
+
+    public TNumber Base => TNumber.MultiplicativeIdentity;
+
+    public override string ToString() {
+        return Postfix;
+    }
+}
+
+public struct SquareMillimeter<TNumber> : IBaseUnit<Meter<TNumber>, TNumber>
+    where TNumber : IMultiplicativeIdentity<TNumber, TNumber>, IParsable<TNumber>
+{
+    public string Postfix => "mm²";
+
+    public TNumber Base => Constants<TNumber>.Number10Em6;
+}
+
+public struct SquareCentimeter<TNumber> : IBaseUnit<Meter<TNumber>, TNumber>
+    where TNumber : IMultiplicativeIdentity<TNumber, TNumber>, IParsable<TNumber>
+{
+    public string Postfix => "cm²";
+
+    public TNumber Base => Constants<TNumber>.Number10Em4;
+}
+
+public struct SquareDecimeter<TNumber> : IBaseUnit<Meter<TNumber>, TNumber>
+    where TNumber : IMultiplicativeIdentity<TNumber, TNumber>, IParsable<TNumber>
+{
+    public string Postfix => "dm²";
+
+    public TNumber Base => Constants<TNumber>.Number10Em2;
+}
+
+public struct SquareKilometer<TNumber> : IBaseUnit<Meter<TNumber>, TNumber>
+    where TNumber : IMultiplicativeIdentity<TNumber, TNumber>, IParsable<TNumber>
+{
+    public string Postfix => "km²";
+
+    public TNumber Base => Constants<TNumber>.Number10E6;
+}
+
+public struct Hectare<TNumber> : IBaseUnit<Meter<TNumber>, TNumber>
+    where TNumber : IMultiplicativeIdentity<TNumber, TNumber>, IParsable<TNumber> {
+    public string Postfix => "ha";
+
+    public TNumber Base => Constants<TNumber>.Number10E4;
+}
+
+public struct SquareMile<TNumber> : IBaseUnit<Meter<TNumber>, TNumber>
+    where TNumber : IMultiplicativeIdentity<TNumber, TNumber>, IParsable<TNumber>
+{
+    public string Postfix => "mile²";
+
+    public TNumber Base => Constants<TNumber>.Dummy;
+}
+
