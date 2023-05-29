2d0f087839d1f3a8d7a952acbdee401c62cd1d49
Selene29 <funmailfach@web.de>
2023-05-29 22:41:00 +0200

    add Substraction & Multiplication

--- Sources/UnitsOfMeasure/Operations.cs
+++ Sources/UnitsOfMeasure/Operations.cs
@@ -1,11 +1,96 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Numerics;
+using System.Runtime.CompilerServices;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace UnitsOfMeasure;
 
-public static class Ops
-{
+/// <summary>
+/// Provides extension methods for performing arithmetic operations like addition, subtraction, and square root on units of measurement.
+/// </summary>
+public static class Ops {
+    /// <summary>
+    /// Performs addition between two units of measurement.
+    /// </summary>
+    /// <typeparam name="T1">The type of the first unit.</typeparam>
+    /// <typeparam name="T2">The type of the second unit.</typeparam>
+    /// <typeparam name="TBase">The base type of both units.</typeparam>
+    /// <typeparam name="TNumber">The type of the numeric value associated with the units.</typeparam>
+    /// <param name="a">The first unit of measurement.</param>
+    /// <param name="b">The second unit of measurement.</param>
+    /// <returns>A new unit representing the addition result.</returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static Unit<Div<T1, T2, T1Base, T2Base, TNumber>, Div<T1Base, T2Base, T1Base, T2Base, TNumber>, TNumber> 
+    public static Unit<T1, TBase, TNumber>
+        Add<T1, T2, TBase, TNumber>(this Unit<T1, TBase, TNumber> a, Unit<T2, TBase, TNumber> b)
+        where TBase : struct, IBaseUnit<TBase, TNumber>
+        where T1 : struct, IBaseUnit<TBase, TNumber>
+        where T2 : struct, IBaseUnit<TBase, TNumber>
+        where TNumber : IDivisionOperators<TNumber, TNumber, TNumber>,
+            IAdditionOperators<TNumber, TNumber, TNumber>,
+            IMultiplyOperators<TNumber, TNumber, TNumber>
+        =>
+            typeof(T1) == typeof(T2)
+            ? new(a.Float + b.Float)
+            : new((a.Float * new T1().Base + b.Float * new T2().Base) / new T1().Base);
+
+    /// <summary>
+    /// Subtracts two units of measurement represented by the given units.
+    /// </summary>
+    /// <typeparam name="T1">The type of the first unit of measurement.</typeparam>
+    /// <typeparam name="T2">The type of the second unit of measurement.</typeparam>
+    /// <typeparam name="TBase">The base unit type for both units of measurement.</typeparam>
+    /// <typeparam name="TNumber">The numeric type used for the values of the units of measurement.</typeparam>
+    /// <param name="a">The first unit of measurement to subtract from.</param>
+    /// <param name="b">The second unit of measurement to subtract.</param>
+    /// <returns>A new unit of measurement representing the result of the subtraction.</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Unit<T1, TBase, TNumber>
+        Subtract<T1, T2, TBase, TNumber>(this Unit<T1, TBase, TNumber> a, Unit<T2, TBase, TNumber> b)
+        where TBase : struct, IBaseUnit<TBase, TNumber>
+        where T1 : struct, IBaseUnit<TBase, TNumber>
+        where T2 : struct, IBaseUnit<TBase, TNumber>
+        where TNumber : IDivisionOperators<TNumber, TNumber, TNumber>,
+            ISubtractionOperators<TNumber, TNumber, TNumber>,
+            IMultiplyOperators<TNumber, TNumber, TNumber>
+        =>
+            typeof(T1) == typeof(T2)
+            ? new(a.Float - b.Float)
+            : new((a.Float * new T1().Base - b.Float * new T2().Base) / new T1().Base);
+
+    /// <summary>
+    /// Multiplies two units of measurement by multiplying their respective float values.
+    /// </summary>
+    /// <typeparam name="T1">The first unit of measurement.</typeparam>
+    /// <typeparam name="T2">The second unit of measurement.</typeparam>
+    /// <typeparam name="T1Base">The base unit type for the first unit.</typeparam>
+    /// <typeparam name="T2Base">The base unit type for the second unit.</typeparam>
+    /// <typeparam name="TNumber">The type of number used for the units.</typeparam>
+    /// <param name="a">The first unit of measurement.</param>
+    /// <param name="b">The second unit of measurement.</param>
+    /// <returns>A new unit representing the product of the two input units.</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Unit<Multi<T1, T2, T1Base, T2Base, TNumber>, Multi<T1Base, T2Base, T1Base, T2Base, TNumber>, TNumber>
+        Multiply<T1, T2, T1Base, T2Base, TNumber>(this Unit<T1, T1Base, TNumber> a, Unit<T2, T2Base, TNumber> b)
+        where T1Base : struct, IBaseUnit<T1Base, TNumber>
+        where T2Base : struct, IBaseUnit<T2Base, TNumber>
+        where T1 : struct, IBaseUnit<T1Base, TNumber>
+        where T2 : struct, IBaseUnit<T2Base, TNumber>
+        where TNumber : IMultiplyOperators<TNumber, TNumber, TNumber>,
+            IDivisionOperators<TNumber, TNumber, TNumber>
+        => new(a.Float * b.Float);
+
+    /// <summary>
+    /// Performs division between two units of measurement.
+    /// </summary>
+    /// <typeparam name="T1">The type of the first unit.</typeparam>
+    /// <typeparam name="T2">The type of the second unit.</typeparam>
+    /// <typeparam name="T1Base">The base type of the first unit.</typeparam>
+    /// <typeparam name="T2Base">The base type of the second unit.</typeparam>
+    /// <typeparam name="TNumber">The type of the numeric value associated with the units.</typeparam>
+    /// <param name="a">The first unit of measurement.</param>
+    /// <param name="b">The second unit of measurement.</param>
+    /// <returns>A new unit representing the division result.</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Unit<Div<T1, T2, T1Base, T2Base, TNumber>, Div<T1Base, T2Base, T1Base, T2Base, TNumber>, TNumber>
          Divide<T1, T2, T1Base, T2Base, TNumber>(this Unit<T1, T1Base, TNumber> a, Unit<T2, T2Base, TNumber> b)
          where T1Base : struct, IBaseUnit<T1Base, TNumber>
          where T2Base : struct, IBaseUnit<T2Base, TNumber>
@@ -15,20 +100,14 @@ public static class Ops
             IMultiplyOperators<TNumber, TNumber, TNumber>
         => new(a.Float / b.Float);
 
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static Unit<T1, TBase, TNumber> 
-        Add<T1, T2, TBase, TNumber>(this Unit<T1, TBase, TNumber> a, Unit<T2, TBase, TNumber> b)
-        where TBase : struct, IBaseUnit<TBase, TNumber>
-        where T1 : struct, IBaseUnit<TBase, TNumber>
-        where T2 : struct, IBaseUnit<TBase, TNumber>
-        where TNumber : IDivisionOperators<TNumber, TNumber, TNumber>, 
-            IAdditionOperators<TNumber, TNumber, TNumber>, 
-            IMultiplyOperators<TNumber, TNumber, TNumber>
-        => 
-            typeof(T1) == typeof(T2)
-            ? new(a.Float + b.Float)
-            : new((a.Float * new T1().Base + b.Float * new T2().Base) / new T1().Base);
-
+    /// <summary>
+    /// Performs a square root operation on a unit of measurement.. set to its base value.
+    /// </summary>
+    /// <typeparam name="T">The type of the unit.</typeparam>
+    /// <typeparam name="TBase">The base type of the unit.</typeparam>
+    /// <typeparam name="TNumber">The type of the numeric value associated with the unit.</typeparam>
+    /// <param name="a">The unit of measurement to perform the square root operation on.</param>
+    /// <returns>A new unit representing the square root result.</returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Unit<Squared<T, TBase, TNumber>, Squared<TBase, TBase, TNumber>, TNumber>
         Square<T, TBase, TNumber>(this Unit<T, TBase, TNumber> a)
@@ -36,5 +115,9 @@ public static class Ops
         where T : struct, IBaseUnit<TBase, TNumber>
         where TNumber : IMultiplyOperators<TNumber, TNumber, TNumber>,
             IDivisionOperators<TNumber, TNumber, TNumber>
-        => new(a.Float * a.Float);
+    => new(a.Float * a.Float);
+
+
+
+
 }
--- Sources/UnitsOfMeasure/PredefinedUnits.Arithmetics.cs
+++ Sources/UnitsOfMeasure/PredefinedUnits.Arithmetics.cs
@@ -1,4 +1,7 @@
-﻿namespace UnitsOfMeasure;
+﻿using System.Numerics;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace UnitsOfMeasure;
 
 public struct Squared<T, TBase, TNumber>
     : IBaseUnit<Squared<TBase, TBase, TNumber>, TNumber>
@@ -17,8 +20,97 @@ public struct Div<T1, T2, T1Base, T2Base, TNumber>
     where T2Base : struct, IBaseUnit<T2Base, TNumber>
     where T1 : struct, IBaseUnit<T1Base, TNumber>
     where T2 : struct, IBaseUnit<T2Base, TNumber>
-    where TNumber : IDivisionOperators<TNumber, TNumber, TNumber>
-{
+    where TNumber : IDivisionOperators<TNumber, TNumber, TNumber> {
     public TNumber Base => new T1().Base / new T2().Base;
-    public string Postfix => $"({new T1().Postfix}/{new T2().Postfix})";
+    public string Postfix => $"({new T1().Postfix} / {new T2().Postfix})";
+    //public string Postfix => HelperStruct<T1, T2, T1Base, T2Base, TNumber>.ModifyPostfix(new T1(), new T2());
+}
+
+public struct Multi<T1, T2, T1Base, T2Base, TNumber>
+    : IBaseUnit<Multi<T1Base, T2Base, T1Base, T2Base, TNumber>, TNumber>
+    where T1Base : struct, IBaseUnit<T1Base, TNumber>
+    where T2Base : struct, IBaseUnit<T2Base, TNumber>
+    where T1 : struct, IBaseUnit<T1Base, TNumber>
+    where T2 : struct, IBaseUnit<T2Base, TNumber>
+    where TNumber : IMultiplyOperators<TNumber, TNumber, TNumber> {
+    public TNumber Base => new T1().Base * new T2().Base;
+    public string Postfix => $"({new T1().Postfix} * {new T2().Postfix})";
+    //public string Postfix => HelperStruct<T1, T2, T1Base, T2Base, TNumber>.ModifyPostfix(new T1(), new T2());
+}
+
+
+
+
+
+
+
+
+
+
+internal struct HelperStruct<T1, T2, T1Base, T2Base, TNumber> {
+    public static string ModifyPostfix(IBaseUnit<T1Base, TNumber> unit1, IBaseUnit<T2Base, TNumber> unit2) {
+        // Get the base and exponent of unit1
+        TNumber base1 = unit1.Base;
+        string postfix1 = unit1.Postfix;
+        Dictionary<string, double> exponents1 = GetExponent(unit1.Postfix);
+
+        // Get the base and exponent of unit2
+        TNumber base2 = unit2.Base;
+        string postfix2 = unit2.Postfix;
+        Dictionary<string, double> exponents2 = GetExponent(unit2.Postfix);
+
+        // Check if both units have the same base
+        if (Equals(postfix1, postfix2)) {
+            // Calculate the new exponent based on the sum of the exponents
+            int newExponent=1;// = exponent1 + exponent2;
+
+            // Reconstruct the postfix with the correct exponent
+            string postfix = ReconstructPostfix(postfix1, newExponent);
+
+            return postfix;
+        }
+
+        // Default case: multiply the postifixes
+        return $"({unit1.Postfix} * {unit2.Postfix})";
+    }
+
+    private static Dictionary<string, double> GetExponent(string postfix) {
+        Dictionary<string, double> exponents = new Dictionary<string, double>();
+
+        // Split the postfix string by whitespace to get individual unit expressions
+        string[] unitExpressions = postfix.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+        foreach (string unitExpression in unitExpressions) {
+            // Split each unit expression by the caret symbol (^)
+            string[] parts = unitExpression.Split('^');
+
+            if (parts.Length == 2) {
+                string baseUnit = parts[0];
+                string exponentString = parts[1];
+
+                if (double.TryParse(exponentString, out double exponent)) {
+                    exponents[baseUnit] = exponent;
+                }
+            } else if (parts.Length == 1) {
+                // If there's no exponent specified, assume it's 1
+                exponents[unitExpression] = 1;
+            } else {
+                // Invalid format, handle accordingly (e.g., throw an exception)
+            }
+        }
+
+        return exponents;
+    }
+
+
+    private static string ReconstructPostfix(string postfix, int exponent) {
+        // Reconstruct the postfix with the correct exponent
+        // Assuming the exponent is represented using ^ symbol
+
+        if (exponent > 1) {
+            return $"{postfix}^{exponent}";
+        } else {
+            return postfix;
+        }
+    }
 }
